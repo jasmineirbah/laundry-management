e291d44a018e9214010dc4f2adff0172e5e839a1
Raihana Jasmine Irbah <jasmineirbah@gmail.com>
2026-06-04 07:05:26 +0700

    Backend deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,7 @@
 const app = require('./app')
 
-const PORT = 3000
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server berjalan di port ${PORT}`
-  )
-})
+  console.log(`Server berjalan di port ${PORT}`);
+});
