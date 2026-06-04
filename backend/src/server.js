4757130ed6c429e0842bddfa145a670677ba44e5
Raihana Jasmine Irbah <jasmineirbah@gmail.com>
2026-06-04 07:52:49 +0700

    Add root route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,20 @@
-const app = require('./app')
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Laundry Backend API Running 🚀"
+  });
+});
+
+// routes
+app.use("/api/customers", customerRoutes);
+app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
