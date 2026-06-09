67a3e4cba5b56c9cb2b8cec9db9bb5fc26593de9
Raihana Jasmine Irbah <jasmineirbah@gmail.com>
2026-06-09 19:17:26 +0700

    use cloud sql socket

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,10 @@ const mysql = require('mysql2/promise')
 require('dotenv').config()
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
